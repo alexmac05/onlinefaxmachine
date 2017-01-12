ace79906b3f56b5b8031c891beb211b55f85260a
Alex McFerron <alexmac2010@gmail.com>
2017-01-11 16:46:30 -0800

    got the Django example working withe the callback outline

--- faxit/faxit.py
+++ faxit/faxit.py
@@ -1,13 +1,13 @@
 import sys
 import os
+import cgi
 
 from django.conf import settings
 
+
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
-
-#SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+CSRF_COOKIE_SECURE = False
 SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
-
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
@@ -22,30 +22,44 @@ settings.configure(
     ),
 )
 
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
-import json
 
-EVENT_OK_RESP_TOKEN = "Hello API Event Received"
 
-def event_callback(request):
-    '''Handles an event callback.
-       Extracts event info, prints it out and return a successful response.
-    '''
+@csrf_exempt
+@require_POST
+def post(request, *args):
+    response = HttpResponse("Hello API Event Received")
+    parseData(request)
+
+    return response
+
+def parseData(request):
+
     try:
-        data = json.loads(request.POST.get('json'))
-        event = data['event']
-        print("ehllo")
+        #form = cgi.FieldStorage()
+        print(request.body)
+
+        #The callback for HelloSign
+        '''
+        #b'------------------------------77934ad02ac3\r\nContent-Disposition: form-data; name="json"\r\n\r\n{"event":{"event_type":"callback_test","event_time":"1484160381","event_hash":"0a7312945ee92921cea0c546d19b8fb180300a62e743647b6b8462bc70c58b67","event_metadata":{"related_signature_id":null,"reported_for_account_id":"f8f805b754bdd2068b8cfb75bb0e2eeb08fbdda2","reported_for_app_id":null,"event_message":null}}}\r\n------------------------------77934ad02ac3--\r\n'
+        '''
+
+        #curl -X POST http://localhost:8000/post/ -d '{"foo":"Bar"}'
+        #curl -X POST http://f3705785.ngrok.io/post/ -d '{"foo":"Bar"}'
+
+
+        #The callback for HelloFax
+
+
     except:
-        print("HELLO")
+        print("Unexpected error:", sys.exc_info()[0])
 
-    return HttpResponse(EVENT_OK_RESP_TOKEN, content_type="html/text")
-
-
-
-
-
+    #print(form)
 
 
 def index(request):
@@ -53,7 +67,7 @@ def index(request):
 
 urlpatterns = (
     url(r'^$', index),
-    url(r'^event_callback', event_callback, name='event_callback'),
+    url(r'^post', post),
 )
 
 application = get_wsgi_application()
